e9ca9ee0dcc96a8d13adaea21356dacd17b380be
pdelpriore <paul.del.priore29@gmail.com>
2021-04-05 17:35:06 +0200

    implement pdf creator

--- src/route/createPdf.ts
+++ src/route/createPdf.ts
@@ -1,28 +1,33 @@
 import { Application, Request, Response } from "express";
 import path from "path";
-import fs from "fs";
 import PdfTemplate from "../template/PdfTemplate";
-import pdf from "html-pdf";
+import puppeteer from "puppeteer";
 
 const createPdf = (app: Application) => {
-  app.post("/createpdf", (req: Request, res: Response) => {
-    const { data } = req.body;
-    pdf
-      .create(PdfTemplate(data), { format: "A4" })
-      .toFile("result.pdf", (err) => {
-        if (err) {
-          return Promise.reject();
-        }
-        const currentFilePath = "./result.pdf";
-        const newFilePath = path.join(__dirname, "..", "file", "result.pdf");
-        fs.rename(currentFilePath, newFilePath, (err) => {
-          if (err) {
-            return Promise.reject();
-          }
-          return Promise.resolve();
-        });
-        return Promise.resolve();
+  app.post("/createpdf", async (req: Request, res: Response) => {
+    try {
+      const { data } = req.body;
+
+      const browser = await puppeteer.launch({ headless: true });
+
+      const page = await browser.newPage();
+      await page.setContent(PdfTemplate(data));
+      await page.emulateMediaType("print");
+      await page.pdf({
+        path: path.join(__dirname, "..", "file", "result.pdf"),
+        format: "a4",
+        preferCSSPageSize: true,
+        printBackground: true,
       });
+
+      await browser.close();
+
+      res.sendStatus(200);
+    } catch (err) {
+      if (err) {
+        res.sendStatus(400);
+      }
+    }
   });
 };
 
--- src/template/PdfTemplate.ts
+++ src/template/PdfTemplate.ts
@@ -8,142 +8,318 @@ const PdfTemplate = (data: string): string => {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Twoje CV</title>
     <style>     
-      .previewColumns {
-        display: flex;
-        flex-direction: row;
-        width: 100vw;
-        height: 100vh;
-      }
-      
-      .firstColumn {
-        display: flex;
-        flex-direction: column;
-        flex: 1;
-        background-color: #2d2d2d;
-      }
-      
-      .secondColumn {
-        flex: 2;
-        background-color: #f3f3f3;
-      }
-      
-      .section {
-        display: flex;
-        flex: 1;
-        flex-direction: column;
-        align-items: baseline;
-        padding: 0.5vw;
-      }
-      
-      .section--photo {
-        align-items: center;
-        justify-content: center;
-      }
-      
-      .section--nophoto {
-        padding: 1vw;
-      }
-      
-      .section__list-firstcolumn {
-        display: grid;
-        grid-template-columns: repeat(3, 1fr);
-      }
-      
-      .section__list-firstcolumn--competence {
-        grid-template-columns: repeat(4, 1fr);
-      }
-      
-      .section__photo {
-        width: 11vw;
-        height: 11vw;
-        object-fit: cover;
-        border: 2px solid #f3f3f3;
-      }
-      
-      .section__photo--volume {
-        width: 9vw;
-        height: 9vw;
-      }
-      
-      .section__label {
-        font-size: 0.8vw;
-        align-self: stretch;
-        text-align: center;
-      }
-      
-      .section__label--firstcolumn {
-        color: #f3f3f3;
-        font-family: "Poppins-Light", sans-serif;
-        border-top: 2px solid #f3f3f3;
-        border-bottom: 2px solid #f3f3f3;
-      }
-      
-      .section__label--secondcolumn {
-        color: #2d2d2d;
-        font-family: "Poppins-Medium", sans-serif;
-        border-top: 2px solid #2d2d2d;
-        border-bottom: 2px solid #2d2d2d;
-      }
-      
-      .section__label--name {
-        text-align: left;
-        font-size: 1.5vw;
-        font-family: "Poppins-Medium", sans-serif;
-        padding-left: 3vw;
-        border-top: none;
-        border-bottom: none;
-      }
-      
-      .section__label--jobposition {
-        font-size: 0.8vw;
-        font-family: "Poppins-Regular", sans-serif;
-      }
-      
-      .github {
-        display: flex;
-        align-items: flex-end;
-        flex-direction: row;
-        padding-top: 0.7vw;
-        padding-bottom: 0.7vw;
-        padding-left: 3vw;
-        padding-right: 3vw;
-      }
-      
-      .github__icon {
-        width: 1vw;
-        height: 1vw;
-        margin-right: 0.3vw;
-      }
-      
-      .github__icon-ico {
-        display: block;
-        font-size: 0.9vw;
-        color: #2d2d2d;
-      }
-      
-      .github__url {
-        font-size: 0.8vw;
-        font-family: "Poppins-Light", sans-serif;
-        color: #2d2d2d;
-        white-space: nowrap;
-        border-bottom: 1px solid #2d2d2d;
-      }
-      
-      .section--rodo {
-        align-items: flex-start;
-        padding-left: 3.5vw;
-        padding-right: 3.5vw;
-      }
-      
-      .section__rodo {
-        font-size: 0.6vw;
-        font-family: "Poppins-Light", sans-serif;
-        color: #2d2d2d;
-        word-wrap: break-word;
-        white-space: pre-wrap;
-        border-top: 1px solid #2d2d2d;
-        border-bottom: 1px solid #2d2d2d;
-      }
+    :root {
+      --width: 100vw;
+      --height: 100vh;
+      --size: 1vw;
+      --primary-color: #f3f3f3;
+      --secondary-color: #2d2d2d;
+    }
+    
+    * {
+        margin: 0;
+        padding: 0;
+    }
+    
+    .previewColumns {
+      display: flex;
+      flex-direction: row;
+      width: var(--width);
+      height: var(--height);
+      overflow: hidden;
+    }
+    
+    .firstColumn {
+      display: flex;
+      flex-direction: column;
+      flex: 1;
+      background-color: var(--secondary-color);
+    }
+    
+    .secondColumn {
+      flex: 2;
+      background-color: var(--primary-color);
+    }
+    
+    .section {
+      display: flex;
+      flex: 1;
+      flex-direction: column;
+      align-items: baseline;
+      padding: 0.5vw;
+    }
+    
+    .section--photo {
+      align-items: center;
+      justify-content: center;
+    }
+    
+    .section--nophoto {
+      padding: var(--size);
+    }
+    
+    .section__list-firstcolumn {
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+    }
+    
+    .section__list-firstcolumn--competence {
+      grid-template-columns: repeat(4, 1fr);
+    }
+    
+    .section__photo {
+      width: 11vw;
+      height: 11vw;
+      object-fit: cover;
+      border: 2px solid var(--primary-color);
+    }
+    
+    .section__photo--volume {
+      width: 9vw;
+      height: 9vw;
+    }
+    
+    .section__label {
+      font-size: 0.8vw;
+      align-self: stretch;
+      text-align: center;
+    }
+    
+    .section__label--firstcolumn {
+      color: var(--primary-color);
+      font-family: "Poppins-Light", sans-serif;
+      border-top: 2px solid var(--primary-color);
+      border-bottom: 2px solid var(--primary-color);
+    }
+    
+    .section__label--secondcolumn {
+      color: var(--secondary-color);
+      font-family: "Poppins-Medium", sans-serif;
+      border-top: 2px solid var(--secondary-color);
+      border-bottom: 2px solid var(--secondary-color);
+    }
+    
+    .section__label--name {
+      text-align: left;
+      font-size: 1.5vw;
+      font-family: "Poppins-Medium", sans-serif;
+      padding-left: 3vw;
+      border-top: none;
+      border-bottom: none;
+    }
+    
+    .section__label--jobposition {
+      font-size: 0.8vw;
+      font-family: "Poppins-Regular", sans-serif;
+    }
+    
+    .github {
+      display: flex;
+      align-items: flex-end;
+      flex-direction: row;
+      padding-top: 0.7vw;
+      padding-bottom: 0.7vw;
+      padding-left: 3vw;
+      padding-right: 3vw;
+    }
+    
+    .github__icon {
+      width: var(--size);
+      height: var(--size);
+      margin-right: 0.3vw;
+    }
+    
+    .github__icon-ico {
+      display: block;
+      font-size: 0.9vw;
+      color: var(--secondary-color);
+    }
+    
+    .github__url {
+      font-size: 0.8vw;
+      font-family: "Poppins-Light", sans-serif;
+      color: #2d2d2d;
+      white-space: nowrap;
+      border-bottom: 1px solid var(--secondary-color);
+    }
+    
+    .section--rodo {
+      align-items: flex-start;
+      padding-left: 3.5vw;
+      padding-right: 3.5vw;
+    }
+    
+    .section__rodo {
+      font-size: 0.6vw;
+      font-family: "Poppins-Light", sans-serif;
+      color: #2d2d2d;
+      word-wrap: break-word;
+      white-space: pre-wrap;
+      border-top: 1px solid var(--secondary-color);
+      border-bottom: 1px solid var(--secondary-color);
+    }
+    
+    .contact {
+      display: flex;
+      align-items: center;
+      flex-direction: row;
+      padding: 0.5vw;
+    }
+    
+    .contact__icon {
+      width: 1vw;
+      height: 1vw;
+      margin-right: 0.3vw;
+    }
+    
+    .contact__icon-ico {
+      display: block;
+      font-size: 0.9vw;
+      color: var(--primary-color);
+    }
+    
+    .contact__content {
+      font-size: 0.8vw;
+      color: var(--primary-color);
+      font-family: "Poppins-ExtraLight", sans-serif;
+      white-space: nowrap;
+    }
+    
+    .diploma {
+      display: flex;
+      flex-direction: column;
+      padding-top: 0.5vw;
+      padding-bottom: 0.5vw;
+      padding-left: 3vw;
+      padding-right: 3vw;
+    }
+    
+    .diploma__content {
+      font-size: 0.8vw;
+      font-family: "Poppins-Light", sans-serif;
+      color: #2d2d2d;
+      white-space: nowrap;
+    }
+    
+    .diploma__content--degree {
+      font-family: "Poppins-Medium", sans-serif;
+    }
+    
+    .diploma__content--description {
+      font-size: 0.7vw;
+      text-align: justify;
+      word-wrap: break-word;
+      white-space: normal;
+    }
+    
+    .experience {
+      display: flex;
+      flex-direction: column;
+      padding-top: 0.5vw;
+      padding-bottom: 0.5vw;
+      padding-left: 3vw;
+      padding-right: 3vw;
+    }
+    
+    .experience__content {
+      font-size: 0.8vw;
+      font-family: "Poppins-Light", sans-serif;
+      color: #2d2d2d;
+      white-space: nowrap;
+    }
+    
+    .experience__content--jobposition {
+      font-family: "Poppins-Medium", sans-serif;
+    }
+    
+    .experience__content--description {
+      font-size: 0.7vw;
+      text-align: justify;
+      word-wrap: break-word;
+      white-space: normal;
+    }
+    
+    .generic {
+      display: flex;
+      align-items: center;
+      flex-direction: row;
+      padding: 0.5vw;
+    }
+    
+    .generic__icon {
+      display: flex;
+      align-items: center;
+      width: 0.9vw;
+      height: 0.9vw;
+      margin-right: 0.3vw;
+    }
+    
+    .generic__icon-ico {
+      width: 0.4vw;
+      height: 0.4vw;
+      border-radius: 50%;
+      background-color: var(--primary-color);
+    }
+    
+    .generic__content {
+      font-size: 0.8vw;
+      color: var(--primary-color);
+      font-family: "Poppins-ExtraLight", sans-serif;
+      white-space: nowrap;
+    }
+    
+    .language {
+      position: relative;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    
+    .language__shape-track {
+      stroke: black;
+      fill: none;
+    }
+    
+    .language__shape-progress {
+      fill: none;
+      stroke: #f3f3f3;
+      stroke-width: 3;
+      transform: rotate(-90deg);
+      transform-origin: center;
+    }
+    
+    .language__flag {
+      position: absolute;
+      width: 1.2vw;
+      height: 1.2vw;
+    }
+    
+    .project {
+      display: flex;
+      flex-direction: column;
+      padding-top: 0.5vw;
+      padding-bottom: 0.5vw;
+      padding-left: 3vw;
+      padding-right: 3vw;
+    }
+    
+    .project__content {
+      font-size: 0.8vw;
+      font-family: "Poppins-Light", sans-serif;
+      color: #2d2d2d;
+      white-space: nowrap;
+    }
+    
+    .project__content--name {
+      font-family: "Poppins-Medium", sans-serif;
+    }
+    
+    .project__content--description {
+      font-size: 0.8vw;
+      text-align: justify;
+      word-wrap: break-word;
+      white-space: pre-wrap;
+    }
     </style>
 </head>
 <body>
