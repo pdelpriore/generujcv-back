1c10c355168805f99565452b9eab585284e7682e
pdelpriore <paul.del.priore29@gmail.com>
2021-04-05 10:26:34 +0200

    add cors + setup a server

--- src/Server.ts
+++ src/Server.ts
@@ -1,6 +1,14 @@
 import express, { Application } from "express"
+import cors from "cors"
 
 const port = process.env.PORT || 4000;
 const app: Application = express()
+
+app.use(cors())
+
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+  }));
 
 app.listen(port, () => console.log(`Server is running on ${port}`))
