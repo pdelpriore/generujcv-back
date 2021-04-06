1f8b984113751948cb3b15054f7cfe6cf0f5ecfc
pdelpriore <paul.del.priore29@gmail.com>
2021-04-06 09:31:37 +0200

    clean code.

--- src/middleware/createPdf.ts
+++ src/middleware/createPdf.ts
@@ -3,35 +3,30 @@ import PdfTemplate from "../template/PdfTemplate";
 import path from "path";
 import puppeteer from "puppeteer";
 
-const createPdf = (app: Application) => {
-  app.post(
-    "/createpdf",
-    async (req: Request, res: Response, next: NextFunction) => {
-      try {
-        const { data, title } = req.body;
+const createPdf = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { data, title } = req.body;
 
-        const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({ headless: true });
 
-        const page = await browser.newPage();
-        await page.setContent(PdfTemplate(data));
-        await page.emulateMediaType("print");
-        await page.pdf({
-          path: path.join(__dirname, "..", "file", `${title}.pdf`),
-          format: "a4",
-          preferCSSPageSize: true,
-          printBackground: true,
-        });
+    const page = await browser.newPage();
+    await page.setContent(PdfTemplate(data));
+    await page.emulateMediaType("print");
+    await page.pdf({
+      path: path.join(__dirname, "..", "file", `${title}.pdf`),
+      format: "a4",
+      preferCSSPageSize: true,
+      printBackground: true,
+    });
 
-        await browser.close();
+    await browser.close();
 
-        next();
-      } catch (err) {
-        if (err) {
-          next(err);
-        }
-      }
+    next();
+  } catch (err) {
+    if (err) {
+      next(err);
     }
-  );
+  }
 };
 
 export default createPdf;
