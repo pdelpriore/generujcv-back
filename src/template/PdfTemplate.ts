99fa0bafcff4030a5749b6479c281a8a470c819d
pdelpriore <paul.del.priore29@gmail.com>
2021-04-07 09:56:48 +0200

    add missing fonts in pdf template

--- src/template/PdfTemplate.ts
+++ src/template/PdfTemplate.ts
@@ -7,6 +7,12 @@ const PdfTemplate = (data: string): string => {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
     <title>Twoje CV</title>
     <style>     
